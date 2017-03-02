4c08fabddc8ba7d041f7e74ea1bc852c3c64a442
Dirk Wildt <wildt@die-netzmacher.de>
2017-03-02 22:15:23 +0100

    2.5.0

---
+++ Configuration/TypoScript/Carousel/Constants/Javascript/_constants.ts
@@ -0,0 +1,3 @@
+<INCLUDE_TYPOSCRIPT: source="FILE:EXT:slick/Configuration/TypoScript/Carousel/Constants/Javascript/constants1.ts">
+<INCLUDE_TYPOSCRIPT: source="FILE:EXT:slick/Configuration/TypoScript/Carousel/Constants/Javascript/constants2.ts">
+<INCLUDE_TYPOSCRIPT: source="FILE:EXT:slick/Configuration/TypoScript/Carousel/Constants/Javascript/constants3.ts">
--- Configuration/TypoScript/Carousel/Constants/_constants.ts
+++ Configuration/TypoScript/Carousel/Constants/_constants.ts
@@ -1,3 +1,3 @@
 <INCLUDE_TYPOSCRIPT: source="FILE:EXT:slick/Configuration/TypoScript/Carousel/Constants/Css/constants.ts">
 <INCLUDE_TYPOSCRIPT: source="FILE:EXT:slick/Configuration/TypoScript/Carousel/Constants/html.ts">
-<INCLUDE_TYPOSCRIPT: source="FILE:EXT:slick/Configuration/TypoScript/Carousel/Constants/Javascript/constants.ts">
+<INCLUDE_TYPOSCRIPT: source="FILE:EXT:slick/Configuration/TypoScript/Carousel/Constants/Javascript/_constants.ts">
