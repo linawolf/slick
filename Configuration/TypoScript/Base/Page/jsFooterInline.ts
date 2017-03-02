de897561a025a082aee9d28a5f7dd14c7b94ab1c
Dirk Wildt <wildt@die-netzmacher.de>
2017-03-02 22:10:54 +0100

    2.5.0

--- Configuration/TypoScript/Base/Page/jsFooterInline.ts
+++ Configuration/TypoScript/Base/Page/jsFooterInline.ts
@@ -5,9 +5,6 @@ page {
 		// slick: 60807, 60808, 60809
   jsFooterInline =
   jsFooterInline {
-      // For proper comment only
-    60807 {
-    }
       // slickcarousel1
     60807 = COA
     60807 {
@@ -92,24 +89,93 @@ page {
 )
       }
     }
-		60808 < .60807
-      // slickcarousel2, #i0018, 17-03-02, dwildt
+      // slickcarousel2 (#i0018, 17-03-02, dwildt, +)
     60808 = COA
     60808 {
+        // Wrap begin
+      10 = TEXT
       10 {
         value (
           jQuery(document).ready(function(){
-						/* #i0018, 17-03-02, dwildt, 1-/+ */
-						/* jQuery('.{$plugin.slick.carousel.css.class}').slick({ */
 						jQuery('#slickcarousel2').slick({
 
 )
       }
-		}
-		60809 < .60807
-      // slickcarousel3, #i0018, 17-03-02, dwildt
+        // Configuration default
+      20 = TEXT
+      20 {
+        value (
+
+                adaptiveHeight  : {$plugin.slick.carousel.2.javascript.properties.adaptiveHeight}
+              , arrows          : {$plugin.slick.carousel.2.javascript.properties.arrows}
+              , autoplay        : {$plugin.slick.carousel.2.javascript.properties.autoplay}
+              , autoplaySpeed   : {$plugin.slick.carousel.2.javascript.properties.autoplaySpeed}
+              , cssEase         : '{$plugin.slick.carousel.2.javascript.properties.cssEase}'
+              , dots            : {$plugin.slick.carousel.2.javascript.properties.dots}
+              , fade            : {$plugin.slick.carousel.2.javascript.properties.fade}
+              , infinite        : {$plugin.slick.carousel.2.javascript.properties.infinite}
+              , initialSlide    : {$plugin.slick.carousel.2.javascript.properties.initialSlide}
+              , rows            : {$plugin.slick.carousel.2.javascript.properties.rows}
+              , slidesPerRow    : {$plugin.slick.carousel.2.javascript.properties.slidesPerRow}
+              , slidesToShow    : {$plugin.slick.carousel.2.javascript.properties.slidesToShow}
+              , slidesToScroll  : {$plugin.slick.carousel.2.javascript.properties.slidesToScroll}
+              , speed           : {$plugin.slick.carousel.2.javascript.properties.speed}
+              , vertical        : {$plugin.slick.carousel.2.javascript.properties.vertical}
+)
+      }
+        // Configuration responsive
+      30 = TEXT
+      30 {
+          // Constant Editor
+        if =
+        if {
+          isTrue = {$plugin.slick.carousel.2.javascript.responsive.if}
+        }
+        value (
+
+              , responsive: [
+                {
+                    breakpoint: {$plugin.slick.carousel.2.javascript.responsive.large.breakpoint}
+                  , settings: {
+                      autoplaySpeed: {$plugin.slick.carousel.2.javascript.responsive.large.settings.autoplaySpeed}
+                    , slidesToShow: {$plugin.slick.carousel.2.javascript.responsive.large.settings.slidesToShow}
+                    , slidesToScroll: {$plugin.slick.carousel.2.javascript.responsive.large.settings.slidesToScroll}
+                  }
+                },
+                {
+                    breakpoint: {$plugin.slick.carousel.2.javascript.responsive.medium.breakpoint}
+                  , settings: {
+                      autoplaySpeed: {$plugin.slick.carousel.2.javascript.responsive.medium.settings.autoplaySpeed}
+                    , slidesToShow: {$plugin.slick.carousel.2.javascript.responsive.medium.settings.slidesToShow}
+                    , slidesToScroll: {$plugin.slick.carousel.2.javascript.responsive.medium.settings.slidesToScroll}
+                  }
+                },
+                {
+                    breakpoint: {$plugin.slick.carousel.2.javascript.responsive.small.breakpoint}
+                  , settings: {
+                      autoplaySpeed: {$plugin.slick.carousel.2.javascript.responsive.small.settings.autoplaySpeed}
+                    , slidesToShow: {$plugin.slick.carousel.2.javascript.responsive.small.settings.slidesToShow}
+                    , slidesToScroll: {$plugin.slick.carousel.2.javascript.responsive.small.settings.slidesToScroll}
+                  }
+                }
+              ]
+)
+      }
+        // Wrap end
+      40 = TEXT
+      40 {
+        value (
+
+            });
+          });
+)
+      }
+    }
+      // slickcarousel3 (#i0018, 17-03-02, dwildt, +)
     60809 = COA
     60809 {
+        // Wrap begin
+      10 = TEXT
       10 {
         value (
           jQuery(document).ready(function(){
@@ -117,7 +183,75 @@ page {
 
 )
       }
-		}
+        // Configuration default
+      20 = TEXT
+      20 {
+        value (
+
+                adaptiveHeight  : {$plugin.slick.carousel.3.javascript.properties.adaptiveHeight}
+              , arrows          : {$plugin.slick.carousel.3.javascript.properties.arrows}
+              , autoplay        : {$plugin.slick.carousel.3.javascript.properties.autoplay}
+              , autoplaySpeed   : {$plugin.slick.carousel.3.javascript.properties.autoplaySpeed}
+              , cssEase         : '{$plugin.slick.carousel.3.javascript.properties.cssEase}'
+              , dots            : {$plugin.slick.carousel.3.javascript.properties.dots}
+              , fade            : {$plugin.slick.carousel.3.javascript.properties.fade}
+              , infinite        : {$plugin.slick.carousel.3.javascript.properties.infinite}
+              , initialSlide    : {$plugin.slick.carousel.3.javascript.properties.initialSlide}
+              , rows            : {$plugin.slick.carousel.3.javascript.properties.rows}
+              , slidesPerRow    : {$plugin.slick.carousel.3.javascript.properties.slidesPerRow}
+              , slidesToShow    : {$plugin.slick.carousel.3.javascript.properties.slidesToShow}
+              , slidesToScroll  : {$plugin.slick.carousel.3.javascript.properties.slidesToScroll}
+              , speed           : {$plugin.slick.carousel.3.javascript.properties.speed}
+              , vertical        : {$plugin.slick.carousel.3.javascript.properties.vertical}
+)
+      }
+        // Configuration responsive
+      30 = TEXT
+      30 {
+          // Constant Editor
+        if =
+        if {
+          isTrue = {$plugin.slick.carousel.3.javascript.responsive.if}
+        }
+        value (
+
+              , responsive: [
+                {
+                    breakpoint: {$plugin.slick.carousel.3.javascript.responsive.large.breakpoint}
+                  , settings: {
+                      autoplaySpeed: {$plugin.slick.carousel.3.javascript.responsive.large.settings.autoplaySpeed}
+                    , slidesToShow: {$plugin.slick.carousel.3.javascript.responsive.large.settings.slidesToShow}
+                    , slidesToScroll: {$plugin.slick.carousel.3.javascript.responsive.large.settings.slidesToScroll}
+                  }
+                },
+                {
+                    breakpoint: {$plugin.slick.carousel.3.javascript.responsive.medium.breakpoint}
+                  , settings: {
+                      autoplaySpeed: {$plugin.slick.carousel.3.javascript.responsive.medium.settings.autoplaySpeed}
+                    , slidesToShow: {$plugin.slick.carousel.3.javascript.responsive.medium.settings.slidesToShow}
+                    , slidesToScroll: {$plugin.slick.carousel.3.javascript.responsive.medium.settings.slidesToScroll}
+                  }
+                },
+                {
+                    breakpoint: {$plugin.slick.carousel.3.javascript.responsive.small.breakpoint}
+                  , settings: {
+                      autoplaySpeed: {$plugin.slick.carousel.3.javascript.responsive.small.settings.autoplaySpeed}
+                    , slidesToShow: {$plugin.slick.carousel.3.javascript.responsive.small.settings.slidesToShow}
+                    , slidesToScroll: {$plugin.slick.carousel.3.javascript.responsive.small.settings.slidesToScroll}
+                  }
+                }
+              ]
+)
+      }
+        // Wrap end
+      40 = TEXT
+      40 {
+        value (
+
+            });
+          });
+)
+      }
+    }
   }
 }
-
