47edd18dd21f270bcf93f70fa78d7676ba3074ff
Dirk Wildt <wildt@die-netzmacher.de>
2017-10-08 18:50:42 +0200

    2.6.0

--- ext_emconf.php
+++ ext_emconf.php
@@ -11,10 +11,10 @@ $EM_CONF[ $_EXTKEY ] = array(
   'uploadfolder' => 0,
   'createDirs' => '',
   'clearcacheonload' => 0,
-  'version' => '2.5.2',
+  'version' => '2.6.0',
   'constraints' => array(
     'depends' => array(
-      'typo3' => '6.0.0-7.6.99',
+      'typo3' => '6.0.0-8.9.99',
     ),
     'conflicts' => array(
     ),
