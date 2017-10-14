98a8d83a693801c3da32641f4857fbed0d57a32c
Dirk Wildt <wildt@die-netzmacher.de>
2017-10-14 22:38:14 +0200

    3.0.0

---
+++ Configuration/ExtTables/6.2/index.php
@@ -0,0 +1,62 @@
+<?php
+
+if ( !defined( 'TYPO3_MODE' ) )
+{
+  die( 'Access denied.' );
+}
+
+use TYPO3\CMS\Backend\Sprite\SpriteManager;
+use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
+
+
+/**
+ * Include TypoScript
+ */
+ExtensionManagementUtility::addStaticFile( $_EXTKEY, 'Configuration/TypoScript/7.6/', 'Slick [1] (based on css_styled_content)' );
+//ExtensionManagementUtility::addStaticFile( $_EXTKEY, 'Configuration/TypoScript/Default', 'Slick [1] (based on fluid_styled_content)' );
+//ExtensionManagementUtility::addStaticFile( $_EXTKEY, 'Configuration/TypoScript/Base', 'Slick [1] Base' );
+//ExtensionManagementUtility::addStaticFile( $_EXTKEY, 'Configuration/TypoScript/Carousel', 'Slick [2] Carousel' );
+//ExtensionManagementUtility::addStaticFile( $_EXTKEY, 'Configuration/TypoScript/Item', 'Slick [3] Carousel Item' );
+//ExtensionManagementUtility::addStaticFile( $_EXTKEY, 'Configuration/TypoScript/RemoveLLATag', 'Slick [9] +Optimise Localisation' );
+
+
+/**
+ * Register Plugins
+ */
+ExtensionManagementUtility::addPlugin(
+        array(
+  'Slick: Carousel 1',
+  'slickcarousel'
+        ), 'CType'
+);
+// #i0018, 17-03-02, dwildt, 6+
+ExtensionManagementUtility::addPlugin(
+        array(
+  'Slick: Carousel 2',
+  'slickcarousel2'
+        ), 'CType'
+);
+// #i0018, 17-03-02, dwildt, 6+
+ExtensionManagementUtility::addPlugin(
+        array(
+  'Slick: Carousel 3',
+  'slickcarousel3'
+        ), 'CType'
+);
+ExtensionManagementUtility::addPlugin(
+        array(
+  'Slick: Carousel Item',
+  'slickitem'
+        ), 'CType'
+);
+
+/**
+ * Page tree icons
+ */
+$TCA[ 'pages' ][ 'columns' ][ 'module' ][ 'config' ][ 'items' ][] = array( 'Slick', 'slick', ExtensionManagementUtility::extRelPath( $_EXTKEY ) . 'ext_icon.gif' );
+SpriteManager::addTcaTypeIcon( 'pages', 'contains-slick', '../typo3conf/ext/slick/ext_icon.gif' );
+
+/**
+ * Page TSConfig
+ */
+require( PATH_typo3conf . 'ext/slick/Configuration/ExtTables/Default/TSconfig/index.php' );
--- ext_tables.php
+++ ext_tables.php
@@ -18,6 +18,8 @@ $typo3Version = $typo3Version + ( ( int ) $bugfix ) * 1;
 switch( TRUE )
 {
 	case($typo3Version < 7006000):
+		require( PATH_typo3conf . 'ext/slick/Configuration/ExtTables/6.2/index.php' );
+		break;
 	case($typo3Version < 8007000):
 		require( PATH_typo3conf . 'ext/slick/Configuration/ExtTables/7.6/index.php' );
 		break;
