8a3f57f046e278c2e8323385c49f721bfa5859a5
Dirk Wildt <wildt@die-netzmacher.de>
2019-02-21 15:17:51 +0100

    4.1.2

--- Configuration/TypoScript/Default/Base/Page/cssInline.ts
+++ Configuration/TypoScript/Default/Base/Page/cssInline.ts
@@ -26,6 +26,43 @@ page {
           font-size: {$plugin.slick.carousel.css.header.font-size};
           font-weight: {$plugin.slick.carousel.css.header.font-weight};
         }
+        /* color */
+        #slickcarousel1 .slick-next:before,
+				#slickcarousel1 .slick-prev:before, 
+        #slickcarousel2 .slick-next:before,
+				#slickcarousel2 .slick-prev:before, 
+        #slickcarousel3 .slick-next:before,
+				#slickcarousel3 .slick-prev:before {
+          color: {$plugin.slick.carousel.css.colors.arrows};
+        }
+        #slickcarousel1 .slick-dots li button:before,
+        #slickcarousel2 .slick-dots li button:before,
+        #slickcarousel3 .slick-dots li button:before {
+          color: {$plugin.slick.carousel.css.colors.dots.default};
+        }
+        #slickcarousel1 .slick-dots li.slick-active button:before,
+        #slickcarousel2 .slick-dots li.slick-active button:before,
+        #slickcarousel3 .slick-dots li.slick-active button:before {
+          color: {$plugin.slick.carousel.css.colors.dots.active};
+        }
+        .slider1 .slick-next:before,
+				.slider1 .slick-prev:before, 
+        .slider2 .slick-next:before,
+				.slider2 .slick-prev:before, 
+        .slider3 .slick-next:before,
+				.slider3 .slick-prev:before {
+          color: {$plugin.slick.carousel.css.colors.arrows};
+        }
+        .slider1 .slick-dots li button:before,
+        .slider2 .slick-dots li button:before,
+        .slider3 .slick-dots li button:before {
+          color: {$plugin.slick.carousel.css.colors.dots.default};
+        }
+        .slider1 .slick-dots li.slick-active button:before,
+        .slider2 .slick-dots li.slick-active button:before,
+        .slider3 .slick-dots li.slick-active button:before {
+          color: {$plugin.slick.carousel.css.colors.dots.active};
+        }
         /* position */
         .slick-dots {
           margin-left:0;
