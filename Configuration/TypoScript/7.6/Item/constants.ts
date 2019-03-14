9755b9c75d308757f3ea8b6c3c4cc3e821f31b91
Dirk Wildt <wildt@die-netzmacher.de>
2019-03-14 09:47:41 +0100

    4.6.3

--- Configuration/TypoScript/7.6/Item/constants.ts
+++ Configuration/TypoScript/7.6/Item/constants.ts
@@ -1,7 +1,7 @@
 plugin.slick {
   item {
     templates {
-			defaults {
+			10 {
 					# cat=Slick - Item Fluidtemplates - Defaults/150/101; type=string; label=templates:Path to the templates (with an ending slash).
 				templateRootPath = EXT:slick/Resources/Private/Fluid/Slick/Templates/
 					# cat=Slick - Item Fluidtemplates - Defaults/150/102; type=string; label=partials:Path to the partials (with an ending slash).
--- Configuration/TypoScript/Default/Deprecated/Item/tt_content/slickitem.ts
+++ Configuration/TypoScript/Default/Deprecated/Item/tt_content/slickitem.ts
@@ -3,15 +3,15 @@ tt_content {
 	slickitem {
 		templateName = Slickitem
 		templateRootPaths {
-			10 = {$plugin.slick.item.templates.defaults.templateRootPath}
+			10 = {$plugin.slick.item.templates.10.templateRootPath}
 			20 = {$plugin.slick.item.templates.20.templateRootPath}
 		}
 		partialRootPaths {
-			10 = {$plugin.slick.item.templates.defaults.partialRootPath}
+			10 = {$plugin.slick.item.templates.10.partialRootPath}
 			20 = {$plugin.slick.item.templates.20.partialRootPath}
 		}
 		layoutRootPaths {
-			10 = {$plugin.slick.item.templates.defaults.layoutRootPath}
+			10 = {$plugin.slick.item.templates.10.layoutRootPath}
 			20 = {$plugin.slick.item.templates.20.layoutRootPath}
 		}
 		settings {
