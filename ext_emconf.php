b2736548313182e2a88e2e6c265bcb79737a7548
Dirk Wildt <wildt@die-netzmacher.de>
2020-10-14 22:20:32 +0200

    [TASK] Improve composer.json

--- ext_emconf.php
+++ ext_emconf.php
@@ -11,10 +11,10 @@ $EM_CONF[ $_EXTKEY ] = array(
   'uploadfolder' => 0,
   'createDirs' => '',
   'clearcacheonload' => 0,
-  'version' => '6.0.5',
+  'version' => '6.0.6',
   'constraints' => array(
     'depends' => array(
-      'typo3' => '6.0.0-10.4.99',
+      'typo3' => '6.0.0-10.9.99',
     ),
     'conflicts' => array(
     ),
