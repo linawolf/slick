a71a55b57945b44c4f9f939134f9d83a01f9a97a
Dirk Wildt <wildt@die-netzmacher.de>
2020-10-14 22:23:06 +0200

    [TASK] Improve composer.json

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,25 +1,25 @@
 <?php
 
 $EM_CONF[ $_EXTKEY ] = array(
-  'title' => 'Slick Slider for TYPO3',
-  'description' => 'Manage content elements, file collections and images with this responsive slider! Based on the slick carousel slider by Ken Wheeler. Scales with its container. Separate settings per breakpoint. Swipe, desktop mouse dragging, infinite looping, randomized order among others. Users can setup more than 50 properties by the plugin.',
-  'category' => 'plugin',
-  'author' => 'Dirk Wildt (Die Netzmacher)',
-  'author_email' => 'http://wildt.at.die-netzmacher.de/',
-  'state' => 'stable',
-  'internal' => '',
-  'uploadfolder' => 0,
-  'createDirs' => '',
-  'clearcacheonload' => 0,
-  'version' => '6.0.6',
-  'constraints' => array(
-    'depends' => array(
-      'typo3' => '6.0.0-10.9.99',
-    ),
-    'conflicts' => array(
-    ),
-    'suggests' => array(
-      'image_crop' => '',
-    ),
-  ),
+		'title' => 'Slick Slider for TYPO3',
+		'description' => 'Manage content elements, file collections and images with this responsive slider! Based on the slick carousel slider by Ken Wheeler. Scales with its container. Separate settings per breakpoint. Swipe, desktop mouse dragging, infinite looping, randomized order among others. Users can setup more than 50 properties by the plugin.',
+		'category' => 'plugin',
+		'author' => 'Dirk Wildt (Die Netzmacher)',
+		'author_email' => 'http://wildt.at.die-netzmacher.de/',
+		'state' => 'stable',
+		'internal' => '',
+		'uploadfolder' => 0,
+		'createDirs' => '',
+		'clearcacheonload' => 0,
+		'version' => '6.0.6',
+		'constraints' => array(
+				'depends' => array(
+						'typo3' => '6.0.0-10.9.99',
+				),
+				'conflicts' => array(
+				),
+				'suggests' => array(
+						'image_crop' => '',
+				),
+		),
 );
