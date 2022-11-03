a5199d1bd8d59fddff1b4ce7bb0984619b705abc
Dirk Wildt <gitlab@die-netzmacher.de>
2022-11-04 00:20:14 +0100

    Last Version for TYPO3 8.7 #t6425

--- ext_emconf.php
+++ ext_emconf.php
@@ -14,7 +14,7 @@ $EM_CONF[ $_EXTKEY ] = array(
   'version' => '6.1.11',
   'constraints' => array(
     'depends' => array(
-      'typo3' => '8.7.0-11.5.99',
+      'typo3' => '8.7.0-8.7.99',
     ),
     'conflicts' => array(
     ),
